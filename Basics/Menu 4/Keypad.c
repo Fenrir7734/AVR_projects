c8bb287d8fe627bd4257c3f563fff114205237b9
Fenrir7734 <hitex1999@gmail.com>
2021-05-25 16:26:32 +0200

    menu 3

---
+++ Basics/Menu 4/Keypad.c
@@ -0,0 +1,173 @@
+#include "Keypad.h"
+
+/*
+Zmienna globalna która będzie ustawiana
+na wartość 1 jeżeli wystąpi przerwanie
+*/
+volatile uint8_t state = 0;
+
+/*
+Procedura obsługi przerwania która odpowiada
+za przerwanie Compare Match. Procedura ta
+zostanie wykonana jeżeli w rejestrze TIMSK 
+została ustawiona flaga OCIE0 oraz w rejestrze
+TIFR flaga OCF0.
+*/
+ISR(TIMER0_COMP_vect) {
+	/*
+	Jeżeli wystąpiło przerwanie wartość zmiennej
+	state jest ustawiana na 1
+	*/
+	state = 1;
+}
+
+/*
+Celem poniżej funkcji jest sprawdzenie  aktualnego 
+stanu  klawiatury i zwrócenie numeru naciśniętego  
+klawisza. Numery zawierają się w przedziale od
+1 do 16. Jeżeli żaden klawisz nie jest naciśnięty
+funkcja zwróci wartość 0. W celu odczytania stanu 
+klawiatury uaktywniam kolejne kolumny a odczytuje 
+stan poszczególnych rzędów.
+*/
+uint8_t keypad() {
+	
+	/*
+	Zmienna która docelowo będzie przechowywać
+	kod naciśniętego klawisza.
+	*/
+	uint8_t key;
+
+	/*
+	Pętla w której odbywa się sprawdzanie
+	stanu klawiatury
+	*/
+	for(uint8_t i = 4; i <= 7; i++) {
+
+		/*
+		Ustawienie na PORTA stanu niskiego
+		na bitach od 4 do 7 co skutkuje 
+		uaktywnieniem kolejnych kolumn.
+		*/
+		PORTA = ~_BV(i);	
+		
+		_delay_ms(1);
+
+		/*
+		Odczytanie stanu PINA i zanegowanie go.
+		Jeżeli jakiś klawisz jest naciśnięty wtedy
+		jedynie 2 bity w zmiennej key będą w stanie
+		wysokim. Głównym powodem tej negacji jest
+		ułatwienie mi napisania dwóch kolejnych
+		warunków.
+		*/
+		key = PINA ^ 0xFF;	
+
+		/*
+		Warunek poprzez wyzerowanie bitów od 4 do 7
+		odpowiadających za kolumny sprawdza czy któryś
+		rząd jest aktywny. Jeżeli tak oznacza to że jakiś
+		przycisk w aktualnie sprawdzanej kolumnie jest 
+		naciśnięty a więc następnie wykona się pętla której
+		celem jest ustalenie w którym rzędzie znajduje się 
+		ten przycisk.
+		Natomiast jeżeli maskowanie da wartość 0 oznacza to
+		że żaden przycisk w tej kolumnie nie jest
+		aktywny a więc główna pętla tej funkcji przejdzie
+		od razu do sprawdzania kolejnej kolumny.
+		*/
+		if(key & 0x0F) {
+
+			/*
+			Pętla której zadaniem jest ustalenie który
+			rząd jest aktywny.
+			*/
+			for(uint8_t j = 0; j <= 3; j++) {
+
+				/*
+				Warunek sprawdza czy bit o numerze j
+				znajduje się w stanie wysokim czyli
+				czy odpowiedni rząd jest aktywny.
+				*/
+				if(key & _BV(j)) {					
+
+					/*
+					Obliczenie numeru naciśniętego przycisku.
+					Obliczenia:
+					przesunięcie + numer_kolumny + 1
+
+					Kolumny i rzędy numeruję od 0 do 3.
+
+					numer_kolumny - numer kolumny w której wykryto 
+					naciśnięty przycisk. Zmienna i ma wartość 
+					odpowiadającą numerowi bitu przyporządkowanego 
+					dla danej kolumny, od 4 do 7. Aby uzyskać numer 
+					kolumny należy odjąć od zmiennej 4.
+					
+					przesunięcie - odpowiada ilości przycisków które
+					należy "przeskoczyć" aby otrzymać numer pierwszego
+					przycisku w interesującym nas rzędzie.
+
+					Dla przykładu jeżeli przycisk znajduje się w rzędzie 
+					nr 2 kolumnie nr 3 to należy przeskoczyć 8 poprzednich
+					przycisków tak aby "wskazać" na pierwszy przycisk w 2
+					rzędzie a następnie dodać do tego numer kolumny w której
+					znajduje się przycisk. Na koniec dodaję do wyniku 1 aby
+					uzyskać numer przycisku z przedziału od 1 do 16 zamiast 
+					od 1 do 15. Czyli::
+					(2 * 4) + (7 - 4) + 1 = 12
+					*/
+					key = (j * 4) + (i - 4) + 1;
+					return key;			
+				}
+			}
+		}
+	}
+
+	/*
+	Jeżeli żadnej przycisk nie jest 
+	wciśnięty funkcja zwraca wartość 0.
+	*/
+	return 0;
+}
+
+/*
+Inicjalizacja timera i przerwań.
+*/
+void init_interrupts() {
+	/*
+    Ustawienie timera w tryb CTC oraz źródła sygnału
+    taktującego na sygnał wewnętrzny wraz z 
+    preskalerem 256
+    */
+    TCCR0 |= _BV(CS02) | _BV(WGM01); 
+
+	/*
+    Uaktywnienie przerwań w timer0. Ustawienie generowania
+    żądania przerwania na Compare Match.
+    */
+	TIMSK |= _BV(OCIE0);
+
+	/*
+    Ustawienie wartości rejestru OCR0.
+        
+    1MHz/256 = 3906Hz
+    1/3906Hz = 256us
+    10ms/256us = 39
+    39 - 1 = 38     <- ponieważ liczymy od 0
+    */
+	OCR0 = 38;
+
+	/*
+    Ustawienie globalnej flagi zezwolenie na przerwanie.
+    */
+	sei();
+}
+
+/*
+Inicjalizacja klawiatury.
+*/
+void init_keypad() {
+    DDRA = 0xF0; 
+	PORTA = 0x0F;
+}
---
+++ Basics/Menu 4/Menu.h
@@ -0,0 +1,109 @@
+#ifndef __MENU_H__
+#define __MENU_H__
+
+#include "LCD.h"
+#include "Functions.h"
+#include "Keypad.h"
+
+#define UP 4
+#define DOWN 8
+#define CLEAR 12
+#define ENTER 16
+
+/*
+Zmienna będąca wskaźnikiem na
+aktualnie wybrany element tj. ten
+przy którym stoi kursor.
+*/
+struct MenuItem* currentItem;
+
+/*
+Zmienna która określa jakiego typu
+jest aktualnie wybrany element. Na
+jej podstawie funkcja menu_select()
+określa czy po wciśnięciu klawisza
+ENTER program przejdzie do wyświetlania
+menu niższego w hierarchii czy rozpocznie
+wykonywanie wybranego z menu programu.
+*/
+volatile enum itemType currentType;
+
+/*
+Definicja typu wyliczeniowego określająca
+charakter elementu menu. Wartość SUBMENU jest
+przyporządkowywana elementom które wskazują
+na kolejne menu, natomiast wartość PROGRAM
+informuje że dany element menu zawiera wskaźnik
+na funkcję (program który zostanie wykonany po
+wyborze tego elementu z menu).
+*/
+enum itemType {SUBMENU, PROGRAM};
+
+/*
+Struktura na podstawie której będą przedstawiane
+kolejne elementy menu. Każde menu jest zorganizowane
+w listę dwukierunkową której węzły stanowią kolejne 
+elementy tego menu. Ponadto każdy element zawiera 
+wskaźnik na element z menu poprzedniego, wskaźnik na 
+pierwszy element z menu następnego, wskaźnik na funkcję
+zawierającą program do wykonania oraz zmienną wyliczeniową
+określającą charakter tego elementu.
+
+name -  tekst wyświetlany na wyświetlaczu LCD opisujący
+        dany element
+parent -wskaźnik do elementu rodzica z menu wyższego w
+        hierarchii. Dzięki niemu możliwy jest powrót do
+        poprzedniego menu
+child - wskaźnik do pierwszego elementu menu potomnego
+        danego elementu. Dzięki niemu możliwe jest przejście
+        do menu niższego w hierarchii. Jeżeli elementu 
+        zawiera wskaźnik na funkcję i zmienna type
+        ma wartość PROGRAM to pole powinno mieć wartość
+        NULL
+next -  wskaźnik na kolejny element tego samego menu.
+prev -  wskaźnik na poprzedni element tego samego menu.
+menu_function - wskaźnik na funkcję zawierającą program
+        do wykonania. Wartość tego pola powinna być NULL
+        jeżeli wartość zmiennej type nie jest PROGRAM
+type -  Zmienna określająca czy dany element zawiera 
+        wskaźnik na menu niższego poziomu czy wskaźnik
+        na funkcję.
+*/
+struct MenuItem
+{
+    char* name;
+    struct MenuItem* parent;
+    struct MenuItem* child;
+    struct MenuItem* next;
+    struct MenuItem* prev;
+    void (*menu_function)(void);
+    enum itemType type;
+};
+
+    /*
+    Deklaracja poszczególnych elementów menu.
+    menuX_Y gdzie:
+    X - numer menu
+    Y - numer elementu menu X
+    */
+    struct MenuItem menu0_1;
+    struct MenuItem menu0_2;
+    struct MenuItem menu0_3;
+    struct MenuItem menu1_1;
+    struct MenuItem menu1_2;
+    struct MenuItem menu2_1;
+    struct MenuItem menu2_2;
+    struct MenuItem menu3_1;
+    struct MenuItem menu3_2;
+	struct MenuItem menu3_3;
+
+void write_menu();
+void menu_cursor_up();
+void menu_cursor_down();
+void menu_select();
+void menu_back();
+void menu_control(uint8_t key);
+uint8_t menu_check_clear();
+void menu_init();
+
+#endif
